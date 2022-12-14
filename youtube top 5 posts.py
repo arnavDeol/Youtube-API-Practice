3d0d495cc529227f6b9150a55f411565afa594b1
Arnav Deol <101570110+arnavDeol@users.noreply.github.com>
2022-12-13 20:17:46 -0500

    Add files via upload

---
+++ youtube top 5 posts.py
@@ -0,0 +1,33 @@
+import requests
+
+# Replace the value of the `api_key` variable with your own YouTube API key
+api_key = "AIzaSyDydR5ONTAyxFt8lcFZn88cOHdgtUuZ67k"
+
+# Replace the value of the `channel_id` variable with the ID of the channel
+# whose top posts you want to retrieve
+channel_id = "UC9DXAsBD4-ITVuvpd68401Q"
+
+# Set the number of posts you want to retrieve
+number_of_posts = 5
+
+# Construct the URL for the API request
+request_url = (
+    f"https://www.googleapis.com/youtube/v3/search?"
+    f"key={api_key}"
+    f"&channelId={channel_id}"
+    f"&part=snippet"
+    f"&maxResults={number_of_posts}"
+    f"&order=date"
+    f"&type=video"
+)
+
+# Send the API request and store the response
+response = requests.get(request_url)
+print(response.status_code)
+
+# Print the title of each post
+if response.status_code == 200:
+    for post in response.json()["items"]:
+        print(post["snippet"]["title"])
+else:
+    print("An error occurred")
